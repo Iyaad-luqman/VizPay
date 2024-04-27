4047e137003242d456f251d1c1a92691e3f75f80
init_6 <iyaadluq@gmail.com>
2024-04-27 06:46:16 +0530

    Small fixes

--- vizpay/lib/main.dart
+++ vizpay/lib/main.dart
@@ -5,61 +5,52 @@ import 'package:local_auth/local_auth.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized(); // Ensure widgets are initialized
   // await Firebase.initializeApp(); // Uncomment and use if Firebase is needed
-
-  final isAuthenticated = await _authenticate(); // Ensure authentication before running the app
-  if (isAuthenticated) {
-    runApp(MyApp());
-  } else {
-    // Handle the case when the user is not authenticated, e.g., exit the app or show an error message
-    runApp(UnauthenticatedApp()); // You need to define UnauthenticatedApp or handle this scenario appropriately
-  }
+  runApp(MyApp());
 }
 
+// Define 'auth' as an instance of LocalAuthentication
 final LocalAuthentication auth = LocalAuthentication();
 
-Future<bool> _checkBiometrics() async {
-  bool canCheckBiometrics = await auth.canCheckBiometrics;
-  return canCheckBiometrics;
-}
-
 Future<bool> _authenticate() async {
-  bool authenticated = false;
+  bool canCheckBiometrics = await auth.canCheckBiometrics;
+  if (!canCheckBiometrics) {
+    print("Biometrics not available or not enrolled.");
+    return false;
+  }
+
   try {
-    authenticated = await auth.authenticate(
+    return await auth.authenticate(
       localizedReason: 'Scan your fingerprint to authenticate',
-      options: const AuthenticationOptions(
-        biometricOnly: true,
-      ),
+      options: const AuthenticationOptions(biometricOnly: true),
     );
   } catch (e) {
     print(e);
+    return false;
   }
-  return authenticated; // Return the authentication status
 }
 
-class MyApp extends StatelessWidget {
-  MyApp({super.key});
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  void initState() {
+    super.initState();
+    _authenticate().then((isAuthenticated) {
+      if (!isAuthenticated) {
+        // Navigate to UnauthenticatedApp or show an error message
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'VizPay',
       theme: ThemeData.dark(),
-      home: HomePage(),
-    );
-  }
-}
-
-// Define UnauthenticatedApp or handle unauthenticated scenario appropriately
-class UnauthenticatedApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Authentication Required'),
-        ),
-      ),
+      home: HomePage(), // Make sure HomePage is ready to handle the case where authentication hasn't occurred yet
     );
   }
 }
